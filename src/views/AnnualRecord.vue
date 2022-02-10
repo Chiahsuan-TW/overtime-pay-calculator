dcb7e5d1d928ae378b1f8f67bacacb8ed8ffb562
Chiahsuan Chou <belinda05021992@gmail.com>
2022-02-10 15:51:48 +0800

    resolve conflict

--- src/views/AnnualRecord.vue
+++ src/views/AnnualRecord.vue
@@ -19,12 +19,12 @@
           <span>2000 NTD</span>
         </p>
       </div>
-      
     </div>
   </div>
 </template>
 <script>
 import Stepper from "@/components/Stepper.vue";
+
 export default {
   name: "AnnualRecord",
   components: {
@@ -38,7 +38,31 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+.modal {
+  max-width: 1000px;
+  background-color: #fff;
+  box-shadow: 3px 6px 14px rgba(255, 153, 0, 0.11);
+  border-radius: 16px;
+  margin: 40px auto 0;
+  padding: 40px 0;
 
+  &-content {
+    width: 100%;
+    padding: 68px;
+  }
 
+  &-content-item {
+    background-color: color.$pale-yellow;
+    display: flex;
+    flex-direction: column;
+    align-items: left;
+    font-size: 0;
+  }
 
+  .monthly-details {
+    span {
+      @extend %content-large;
+    }
+  }
+}
 </style>
