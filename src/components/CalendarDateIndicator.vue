7485c303175be188c1461d3a802e05958fbd40c4
Chiahsuan Chou <belinda05021992@gmail.com>
2022-02-14 15:22:54 +0800

    finish monthly record page layout

---
+++ src/components/CalendarDateIndicator.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="date-indicator">
+    <span @click="selectPreviousYear" class="select">«</span>
+    <span @click="selectPreviousMonth" class="select">‹</span>
+    <span class="calendar-date">{{ selectedMonth }}</span>
+    <span @click="selectNextMonth" class="select">›</span>
+    <span @click="selectNextYear" class="select">»</span>
+  </div>
+</template>
+
+<script>
+import dayjs from "dayjs";
+
+export default {
+  props: {
+    selectedDate: {
+      type: Object,
+      required: true,
+    },
+    // currentDate: {
+    //   type: String,
+    //   required: true,
+    // },
+  },
+  methods: {
+    selectPreviousYear() {
+      let newSelectedDate = dayjs(this.selectedDate).subtract(1, "year");
+      this.$emit("dateSelected", newSelectedDate);
+    },
+    selectPreviousMonth() {
+      let newSelectedDate = dayjs(this.selectedDate).subtract(1, "month");
+      this.$emit("dateSelected", newSelectedDate);
+    },
+    selectNextYear() {
+      let newSelectedDate = dayjs(this.selectedDate).add(1, "year");
+      this.$emit("dateSelected", newSelectedDate);
+    },
+    selectNextMonth() {
+      let newSelectedDate = dayjs(this.selectedDate).add(1, "month");
+      this.$emit("dateSelected", newSelectedDate);
+    },
+  },
+  computed: {
+    selectedMonth() {
+      return this.selectedDate.format("MMMM YYYY");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.date-indicator {
+  text-align: center;
+  padding: 10px 5px;
+}
+.select {
+  font-size: 25px;
+}
+.select:hover {
+  cursor: pointer;
+  color: color.$primary-dark;
+}
+
+.calendar-date {
+  @extend %content-title;
+  padding: 0 10px;
+  display: inline-block;
+  width: 200px;
+}
+</style>
